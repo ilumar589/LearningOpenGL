01e78212d19badbe1cf9cc30cc369b99062b889a
eduard.parvu <parvueduard@gmail.com>
2018-05-14 07:19:45 +0300

    - just a window :)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+const int WINDOW_WIDTH = 800;
+const int WIDOW_HEIGHT = 600;
+
+
+void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void draw2DTriangle() {
+    float vertices[] {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WIDOW_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        throw "Failed to create GLFW window";
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WINDOW_WIDTH, WIDOW_HEIGHT);
+
+    glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+    // render loop
+    while (!glfwWindowShouldClose(window)) {
+
+        // input
+        processInput(window);
+
+        // rendering commands here
+
+        // check and call events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    glfwTerminate();
     return 0;
 }
+
+
