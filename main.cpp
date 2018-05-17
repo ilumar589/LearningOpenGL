c950efaa5a874ca38bc43b38259c1f0580e0f76b
eduard.parvu <parvueduard@gmail.com>
2018-05-17 07:31:23 +0300

    non working version

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <tuple>
 #include "headers/Utility/Utility.h"
 
 
 const int WINDOW_WIDTH = 800;
 const int WIDOW_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\n\0";
+
 
 
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
@@ -18,24 +33,7 @@ void processInput(GLFWwindow* window) {
     }
 }
 
-unsigned int setup2DTriangle() {
-    float vertices[] {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
-    };
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    const char *vertexShaderSource = "#version 330 core\n"
-                                        "layout (location = 0) in vec3 aPos; \n"
-                                        "\n"
-                                        "void main() {\n"
-                                        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n"
-                                        "}\0";  // this will be moved to a glsl file
+unsigned int setupShaders() {
 
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -44,12 +42,7 @@ unsigned int setup2DTriangle() {
 
     project_utility::shaderInfo(vertexShader);
 
-    const char *fragmentShaderSource = "#version 330 core\n"
-                                          "out vec4 FragColor;\n"
-                                          "\n"
-                                          "void main() {\n"
-                                          "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                          "}\0"; // \0 pff c strings
+
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
@@ -70,6 +63,22 @@ unsigned int setup2DTriangle() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    return shaderProgram;
+}
+
+std::tuple<unsigned int, unsigned int> setup2DTriangle() {
+    float vertices[] {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     // right now we've sent the gpu vertex data and instructed it how to process
     // that data with a vertex and fragment shader but it has no idea how it should
     // link the vertex data to the actual vertex shader
@@ -77,9 +86,14 @@ unsigned int setup2DTriangle() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    return shaderProgram;
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-//    glUseProgram(shaderProgram);
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+    return std::make_tuple(VBO, VAO);
 }
 
 int main() {
@@ -106,7 +120,11 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
-    unsigned int shaderProgram = setup2DTriangle();
+    unsigned int shaderProgram = setupShaders();
+    std::tuple<unsigned int, unsigned int> vbo_and_vao = setup2DTriangle();
+
+    unsigned int vbo = std::get<0>(vbo_and_vao);
+    unsigned int vao = std::get<1>(vbo_and_vao);
 
     // render loop
     while (!glfwWindowShouldClose(window)) {
@@ -114,15 +132,24 @@ int main() {
         // input
         processInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // rendering commands here
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // check and call events and swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
     }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
 
     glfwTerminate();
 
